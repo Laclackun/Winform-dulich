0533e607176d828b1911097ebf828adaf186c06e
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2024-11-07 18:47:01 +0700

    InforModel.cs

--- TravelTour/TravelTour/Model/InforModel.cs
+++ TravelTour/TravelTour/Model/InforModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,16 +15,27 @@ namespace TravelTour.Model
         public string Location { get; set; }
         public string Sex { get; set; }
         public int Age { get; set; }
-
+        
         public InforModel()
         {
             ID = 0;
-            Name = "";
-            ImageUrl = "";
+            Name = string.Empty;
+            ImageUrl = string.Empty;
             Date = DateTime.MinValue;
-            Location = "";
-            Sex = "";
+            Location = string.Empty;
+            Sex = string.Empty;
             Age = 0;
+        }
+        
+        public InforModel(int id, string name, string imageUrl, DateTime date, string location, string sex, int age)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.ImageUrl = imageUrl;
+            this.Date = date;
+            this.Location = location;
+            this.Sex = sex;
+            this.Age = age;
         }
     }
 }
